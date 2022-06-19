d6684d3f8478ade3979f71109bb95e96f2d1db47
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-06-19 12:02:25 -0500

    refactor file

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -8,7 +8,5 @@ export const exerciseOptions = {
 
 export const fetchData = async (url, options)=>{
     const response = await fetch(url, options);
-    const data = await response.json();
-
-    return data;
+    return await response.json();
 }
