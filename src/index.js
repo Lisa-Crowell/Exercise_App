7e99372846a9e83a0c3f0a137e5faa3810ebb518
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-06-19 13:56:23 -0500

    update  no-inspect

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {BrowserRouter} from "react-router-dom";
 import App from './App';
 
 
+// noinspection JSCheckFunctionSignatures
 const root = ReactDom.createRoot(document.getElementById('root'));
 
 root.render(
