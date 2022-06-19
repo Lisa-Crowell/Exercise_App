2751b050102d49c615f5c8859cb2e14b5d521a4a
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-06-19 11:04:26 -0500

    configure environment variables

---
+++ src/utils/fetchData.js
@@ -0,0 +1,14 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': process.env.REACT_APP_RAPID_API_HOST,
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
+    }
+};
+
+export const fetchData = async (url, options)=>{
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    return data;
+}
