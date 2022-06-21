09de8db3d5addeaabc4ece2dfc6a5f2aba8a6cbd
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-06-20 23:31:45 -0500

    update useEffect

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -37,7 +37,7 @@ const ExerciseDetail = () => {
 
         // noinspection JSIgnoredPromiseFromCall
         fetchExercisesData();
-    }, []);
+    }, [id]);
 
     if (!exerciseDetail) return <div>No Data</div>;
 
