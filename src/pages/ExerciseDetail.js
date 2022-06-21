d3c5f0b97bd62ccc146c4003d5570c7a62bc06a3
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-06-20 21:17:10 -0500

    implement ExerciseDetail.js

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useParams} from "react-router-dom";
+import {Box} from '@mui/material';
 
-const ExerciseDetail = () =>{
+import {exerciseOptions, fetchData, youtubeOptions} from "../utils/fetchData";
+
+import Detail from "../components/Detail";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises";
+
+const ExerciseDetail = () => {
+    const [exerciseDetail, setExerciseDetail] = useState({});
+    const [exerciseVideos, setExerciseVideos] = useState([]);
+    const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
+    const [equipmentExercises, setEquipmentExercises] = useState([]);
+    const { id } = useParams();
+
+    useEffect(()=>{
+        window.scrollTo({top: 0, behavior:'smooth'});
+
+        const fetchExercisesData = async () => {
+            const exerciseDbUrl = process.env.REACT_APP_RAPID_API_HOST;
+            const youtubeSearchUrl = process.env.REACT_APP_RAPID_API_YOUTUBE_SEARCH;
+
+            const exerciseDetailData = await fetchData(`https://${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
+            setExerciseDetail(exerciseDetailData);
+
+            const exerciseVideosData = await fetchData(`https://${youtubeSearchUrl}/search?query=${exerciseDetailData.name} exercise`, youtubeOptions);
+            setExerciseVideos(exerciseVideosData.contents);
+
+            const targetMuscleExercisesData = await fetchData(`https://${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions);
+            setTargetMuscleExercises(targetMuscleExercisesData);
+
+            const equipmentExercisesData = await fetchData(`https://${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`, exerciseOptions);
+            setEquipmentExercises(equipmentExercisesData);
+        }
+
+        // noinspection JSIgnoredPromiseFromCall
+        fetchExercisesData();
+    }, []);
+
+    if (!exerciseDetail) return <div>No Data</div>;
+
+
     return(
-        <div>Exercise Detail</div>
+        <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
+            <Detail exerciseDetail={exerciseDetail} />
+            <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
+            <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} />
+        </Box>
     )
 }
 
